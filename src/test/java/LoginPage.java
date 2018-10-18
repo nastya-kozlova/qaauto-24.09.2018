23fb4f8617f4285acb31f6b0d4a7ce189aacbba1
nastya <as.arsenicum@gmail.com>
2018-10-18 20:41:13 +0300

    Page object Home - Partial commit

--- src/test/java/LoginPage.java
+++ src/test/java/LoginPage.java
@@ -4,30 +4,29 @@ import org.openqa.selenium.WebElement;
 
 public class LoginPage {
     WebDriver webDriver;//объявили
-    WebElement logInEmailField ;
+    WebElement logInEmailField;
     WebElement passwordDataField;
-    WebElement signInButton ;
+    WebElement signInButton;
 
     public LoginPage(WebDriver webDriver) { //конструктор. работает после инициализации переменных
         this.webDriver = webDriver;
         initElements();
     }
 
-public void initElements(){
-    logInEmailField = webDriver.findElement(By.xpath("//input[@id='login-email']"));
-    passwordDataField = webDriver.findElement(By.xpath("//input[@id='login-password']"));
-    signInButton = webDriver.findElement(By.xpath("//input[@id='login-submit']"));
+    public void initElements() {
+        logInEmailField = webDriver.findElement(By.xpath("//input[@id='login-email']"));
+        passwordDataField = webDriver.findElement(By.xpath("//input[@id='login-password']"));
+        signInButton = webDriver.findElement(By.xpath("//input[@id='login-submit']"));
 
 
-}
-public void login(String userEmail, String userPassword){
+    }
+
+    public void login(String userEmail, String userPassword) {
         logInEmailField.sendKeys(userEmail);
         passwordDataField.sendKeys(userPassword);
         signInButton.click();
 
     }
-
-
 
 
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -8,21 +8,23 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-    WebDriver webDriver;//лбъявили переменную
+    WebDriver webDriver;//объявили переменную
+
     @BeforeMethod
-    public void beforeMethod () {
+    public void beforeMethod() {
         webDriver = new FirefoxDriver();//проинициализировали переменную
 
     }
+
     @AfterMethod
-    public void afterMethod () {
+    public void afterMethod() {
         webDriver.quit();
     }
 
     /**
      * PreConditions:
      * - Open FF browser.
-     *
+     * <p>
      * Scenario:
      * - Navigate to https://linkedin.com.
      * - Verify that Login page is loaded.
@@ -30,10 +32,11 @@ public class LoginTest {
      * - Enter userPassword into userPassword field.
      * - Click on signIn button.
      * - Verify that Home page is loaded.
-     *
+     * <p>
      * PostCondition:
      * - Close FF browser.
      */
+
     @Test
     public void successfulLoginTest() {
 
@@ -42,39 +45,48 @@ public class LoginTest {
 
         Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/",
                 "Home page URL is wrong.");
-        loginPage.login( "nastya_kozlova@hotmail.com", "1992mypas");
+        Assert.assertEquals(webDriver.getTitle(), "LinkedIn: Войти или зарегистрироваться", "Login page title is wrong.");
+        Assert.assertTrue(loginPage.signInButton.isDisplayed(), "SignIn Button is not displayed on Login Page");
+        loginPage.login("nastya_kozlova@hotmail.com", "1992mypas");
 
 
         Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/feed/", "Homepage URL is wrong");
+        Assert.assertEquals(webDriver.getTitle(), "LinkedIn", "Home page title is wrong.");
+        //li[@id='profile-nav-item']
+
+        HomePage homePage = new HomePage(webDriver);
+
+        Assert.assertTrue(homePage.profileNavItem.isDisplayed(), "profileNavItem is not displayed on Login Page");
+
 
     }
+
     @Test
-    public void  negativeLoginTest () {
+    public void negativeLoginWithEmptyPasswordTest() {
         webDriver.get("https://linkedin.com");
         LoginPage loginPage = new LoginPage(webDriver);
 
         Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/",
                 "Home page URL is wrong.");
-        loginPage.login("", "");
+        loginPage.login("", "");//вызов метода логин, который есть в классе логинпейдж.джава
 
 
         Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/feed/", "Homepage URL is wrong");
 
     }
 
-@Test
-    public void wrongPasswordTest () {
+    @Test
+    public void wrongPasswordTest() {
         webDriver.get("https://linkedin.com");
         LoginPage loginPage = new LoginPage(webDriver);
 
-    Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/",
-            "Home page URL is wrong.");
-    loginPage.login("nastya_kozlova@hotmail.com", "658hsgdsd");
+        Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/",
+                "Home page URL is wrong.");
+        loginPage.login("nastya_kozlova@hotmail.com", "658hsgdsd");
 
-    Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/uas/login-submit?loginSubmitSource=GUEST_HOME", "Homepage URL is wrong");
+        Assert.assertEquals(webDriver.getCurrentUrl(), "https://www.linkedin.com/uas/login-submit?loginSubmitSource=GUEST_HOME", "Homepage URL is wrong");
 
-}
-
+    }
 
 
 }
